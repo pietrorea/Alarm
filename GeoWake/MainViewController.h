dfda7affd8a51413fd50d8b12f9e3562b77aa878
Pietro Rea <pietrorea@gmail.com>
2012-04-28 12:56:43 -0400

    created new XCode project, deleted old

---
+++ GeoWake/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  GeoWake
+//
+//  Created by Pietro Rea on 4/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
