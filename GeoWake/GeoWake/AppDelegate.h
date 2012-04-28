6f1695440ede11170a089e27529686290bd89c7c
Pietro Rea <pietrorea@gmail.com>
2012-04-28 12:17:13 -0400

    created XCode project

---
+++ GeoWake/GeoWake/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GeoWake
+//
+//  Created by Pietro Rea on 4/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GeoWake/GeoWake/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GeoWake
+//
+//  Created by Pietro Rea on 4/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
